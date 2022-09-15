cc7e4fc203e781ffc1e5f3e5b02526991c7694f3
d-p35 <dhruv10203@gmail.com>
2022-09-15 03:04:28 -0400

    Importing Emojipedia project

---
+++ Emojipedia/src/emojipedia.js
@@ -0,0 +1,47 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "😉",
+    name: "Winking Face",
+    meaning:
+      "A yellow face with a slight smile or open mouth shown winking, usually its left eye. May signal a joke, flirtation, hidden meaning, or general positivity. Tone varies, including playful, affectionate, suggestive, or ironic."
+  },
+  {
+    id: 2,
+    emoji: "💪",
+    name: "Flexed Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 3,
+    emoji: "🤪",
+    name: "Zany Face",
+    meaning:
+      "A yellow face with its head tilted, its tongue hanging out of a big grin, and wide eyes in a wild, cockeyed expression. Generally used to express silliness."
+  },
+  {
+    id: 4,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 5,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+
+  {
+    id: 6,
+    emoji: "😲",
+    name: "Astonished Face",
+    meaning:
+      "A yellow face with open eyes, raised eyebrows, and an open mouth, as if gasping in shock or surprise. Often depicted showing its upper teeth. May convey a wide range of emotions, including awe, amazement, admiration, disbelief, excitement, or concern."
+  }
+];
+
+export default emojipedia;
